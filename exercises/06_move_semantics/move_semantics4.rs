8a892fc0c3759a0dfdc87affa1e7c8b26264995c
Tristan Kalvoda <kalvo007@morris.umn.edu>
2025-01-29 21:40:31 -0600

    completed 06

--- exercises/06_move_semantics/move_semantics4.rs
+++ exercises/06_move_semantics/move_semantics4.rs
@@ -8,10 +8,10 @@ mod tests {
     // Don't add, change or remove any line.
     #[test]
     fn move_semantics4() {
-        let mut x = Vec::new();
+        let mut  x = Vec::new();
         let y = &mut x;
-        let z = &mut x;
         y.push(42);
+        let z = &mut x;
         z.push(13);
         assert_eq!(x, [42, 13]);
     }
